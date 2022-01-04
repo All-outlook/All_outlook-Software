eb9f48aca2ec506d349a24b1a5ae491688c91837
fTomo-robot <96418441+fTomo-robot@users.noreply.github.com>
2022-01-04 20:52:51 +0900

    本日の頑張り(Jun)

--- Jun-Arduino/通信確認用/UART_Receive/UART_Receive.ino
+++ Jun-Arduino/通信確認用/UART_Receive/UART_Receive.ino
@@ -1,12 +1,22 @@
-/**********************************************
-  Arduino UART Receive (Master)
-**********************************************/
+//void setup() {
+//  Serial.begin(115200);
+//  Serial3.begin(38400);
+//  pinMode(8, INPUT);
+//
+//}
+//
+//void loop() {
+//  if (Serial3.available()) {
+//    Serial.println(Serial3.read());
+//  }
+//}
+
 
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(8, 18); // RX, TX
+SoftwareSerial mySerial(66, 69); // RX, TX
 
 void setup() {
-  Serial.begin(38400);
+  Serial.begin(115200);
   mySerial.begin(38400);
   pinMode(8, INPUT);
 
@@ -15,5 +25,8 @@ void setup() {
 void loop() {
   if (mySerial.available()) {
     Serial.println(mySerial.read());
+  }
+  for (int i = 0; i <= 100; i++) {
+    mySerial.write(i);
   }
 }
